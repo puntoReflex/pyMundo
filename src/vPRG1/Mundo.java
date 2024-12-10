8bf3edb85be4b788e61bfb1ecb4e51cec002e8ee
Manuel Masías <mmasias@users.noreply.github.com>
2024-12-10 11:31:53 +0100

    Personaje y movimiento

--- src/vPRG1/Mundo.java
+++ src/vPRG1/Mundo.java
@@ -1,5 +1,7 @@
 package vPRG1;
 
+import java.util.Scanner;
+
 class Mundo {
     public static void main(String[] args) {
         int[][] miniMap = {
@@ -23,30 +25,57 @@ class Mundo {
                 { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 6, 6, 8, 8, 8, 8, 1, 0, 0, 0 },
                 { 7, 7, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 6, 6, 8, 8, 8, 8, 1, 0, 5, 0 }
         };
-        printMap(miniMap);
+        int[] posicion = {5, 5};
+        boolean isPlaying = true;
+
+        do {
+            printMap(miniMap, posicion);
+            isPlaying = getInput(posicion);
+        } while (isPlaying);
+
     }
 
-    static void printMap(int[][] map) {
+    static boolean getInput(int[] posicion) {
+        String movement = "" + new Scanner(System.in).nextLine().charAt(0);
+        switch (movement) {
+            case "a" -> posicion[0] = posicion[0] - 1;
+            case "d" -> posicion[0] = posicion[0] + 1;
+            case "w" -> posicion[1] = posicion[1] - 1;
+            case "s" -> posicion[1] = posicion[1] + 1;
+            case "q" -> {return false;}
+            default -> {}
+        };
+        return true;
+    }
+
+    static void printMap(int[][] map, int[] posicion) {
+        final int HERO = 99;
         for (int row = 0; row < map.length; row++) {
             for (int column = 0; column < map[row].length; column++) {
-                System.out.print(parse(map[row][column]));
+                if (row == posicion[0] && column == posicion[1]) {
+                    System.out.print(parse(HERO));
+                } else {
+                    System.out.print(parse(map[row][column]));
+                }
+
             }
             System.out.println();
         }
     }
 
-    static String parse(int value){
-        return switch(value){
+    static String parse(int value) {
+        return switch (value) {
             case 0 -> "   ";
             case 1 -> "[#]";
             case 2 -> " · ";
             case 3 -> "~ ~";
             case 4 -> "'''";
-            case 5 -> ":::"; 
+            case 5 -> ":::";
             case 6 -> ", '";
             case 7 -> "/^\\";
             case 8 -> "^Y^";
             case 9 -> ":|:";
+            case 99 -> "_O_";
             default -> "???";
         };
     }
